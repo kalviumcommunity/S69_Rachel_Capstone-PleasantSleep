4de934a57da3f7c21f58372453a13c450eb5bee3
Lenita-1201 <rachel.lenita1201@gmail.com>
2025-12-26 23:14:16 +0530

    feat: add POST API endpoint /api/users

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const users = [];
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,10 +16,32 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+
 app.get("/api/users", (req, res) => {
   res.status(200).json({
-    users: [],
+    success: true,
+    users,
     message: "User list fetched successfully"
+  });
+});
+
+app.post("/api/users", (req, res) => {
+  const { username, email } = req.body;
+
+  if (!username || !email) {
+    return res.status(400).json({ 
+      success: false, 
+      message: "Missing username or email" 
+    });
+  }
+
+  const newUser = { username, email };
+  users.push(newUser);
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    user: newUser
   });
 });
 
